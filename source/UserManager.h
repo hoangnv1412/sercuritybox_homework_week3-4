cb3f1556f4baa77fa8664ed581ecf1e4351e0176
DESKTOP-O5UFE3V\hoang <hoangnv1412@gmail.com>
2018-08-07 11:48:45 +0700

    a
    a

--- source/UserManager.h
+++ source/UserManager.h
@@ -109,7 +109,18 @@ public:
 	}
 
 	void showMess() {
-
+		openDb();
+		string sql = "SELECT MESSAGE.* FROM MESSAGE,USER WHERE USER.username = '" + username +"';";
+			
+		cout << "sql : " << sql << endl;
+		int rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else {
+		}
+		sqlite3_close(db);
 	}
 
 	void showDetailMess() {
@@ -117,15 +128,84 @@ public:
 	}
 
 	void addFriend() {
+		openDb();
+		string frien;
+		
+		cout << "Enter username: ";
+		cin >> frien;
+		bool isEx = isExisted(frien);
+		bool isBl = isBlocked(username, frien);
+		bool isFr = isFriend(username, frien);
 
+		if (!isEx) cout << "ERROR : user does not exist" << endl;
+		else if (isBl) cout << "ERROR : block cmnr" << endl;
+		else if (isFr) cout << "You are already friend" << endl;
+		if (isEx && !isBl && !isFr) {
+			string sql = "INSERT INTO FRIEND (user1,user2) " \
+				"VALUES('" + username + "','"  + frien + "')";
+			cout << "sql : " << sql << endl;
+			int rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+			if (rc != SQLITE_OK) {
+				fprintf(stderr, "SQL error: %s\n", zErrMsg);
+				sqlite3_free(zErrMsg);
+			}
+			else {
+				fprintf(stdout, "Insert success\n");
+			}
+			cout << "Add friend success" << endl;
+		}
+		
+		sqlite3_close(db);
 	}
 
 	void showFriendList() {
+		openDb();
+		string sql = "SELECT friend.* FROM friend,user WHERE USER.username = '" + username + "';";
 
+		cout << "sql : " << sql << endl;
+		int rc = sqlite3_exec(db, sql.c_str(), callback_frl, 0, &zErrMsg);
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else {
+		}
+		cout << "\n\n";
+		sqlite3_close(db);
 	}
 
 	void Block() {
+		string victim;
+		openDb();
+		cout << "Enter name you want to block : ";
+		cin >> victim;
+		clear_buffer();
+		bool isEx = isExisted(victim);
+		bool isBl = isBlocked(username, victim);
+		bool isFr = isFriend(username, victim);
+		if (!isEx) cout << "ERROR : user does not exist" << endl;
+		else if (isBl) cout << "ERROR : Username is already blocked" << endl;
+		else if (isFr) cout << "Username is in your friendlist" << endl;
+		if (isEx && !isBl) {
+			cout << "Are you sure you want to block this person?\nEnter Y for Yes, N for No : ";
+			char ch = getchar();
+			while ((ch != 'Y') && (ch != 'N') && (ch != 'n') && (ch != 'y')) {
+				cout << "Wrong syntax. Please try again" << endl;
+				cout << "Enter Y for Yes, N for No : ";
+				ch = getchar();
+			}
 
+			if ((ch == 'N') || (ch == 'n')) {
+				cout << "Okay\n";
+				return;
+			}
+			else {
+				BlockExe(victim);
+				if (isFr) Unfriend(username, victim);
+			}
+			
+		}
+		sqlite3_close(db);
 	}
 
 	~UserManager();
@@ -140,12 +220,22 @@ private:
 	sqlite3 * db;
 	char *zErrMsg = 0;
 
+	static int callback_frl(void *data, int argc, char **argv, char **azColName) {
+		int i;
+		fprintf(stderr, "id\tuser1\tuser2\n", (const char*)data);
+		for (i = 0; i<argc; i++) {
+			printf("%s\t", argv[i] ? argv[i] : "NULL");
+		}
+		printf("\n");
+		return 0;
+	}
+
 	static int callback(void *data, int argc, char **argv, char **azColName) {
 		int i;
 		fprintf(stderr, "%s: ", (const char*)data);
-		/*for (i = 0; i<argc; i++) {
+		for (i = 0; i<argc; i++) {
 			printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-		}*/
+		}
 		printf("\n");
 		return 0;
 	}
@@ -155,7 +245,7 @@ private:
 		openDb();
 		string sql = "INSERT INTO MESSAGE (sender,receiver,content,time) " \
 			"VALUES('" + sender + "','" + receiver + "','" + content + "','" + dt +  "')";
-		cout << "sql : " << sql << endl;
+		//cout << "sql : " << sql << endl;
 		int rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
 		if (rc != SQLITE_OK) {
 			fprintf(stderr, "SQL error: %s\n", zErrMsg);
@@ -181,29 +271,68 @@ private:
 
 	}
 
+	void BlockExe(string victim) {
+		
+		string sql = "INSERT INTO BLOCK (blocker,blocked) " \
+			"VALUES('" + username + "','" + victim + "')";
+		//cout << "sql : " << sql << endl;
+		int rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else {
+			
+		}
+		
+	}
+
+	void Unfriend(string username, string victim) {
+		
+		string sql = "DELETE FROM FRIEND " \
+			"WHERE ( FRIEND.user1 = '" \
+			+ username + "' AND FRIEND.user2 = '" + victim \
+			+ "' ) OR ( FRIEND.user1 = '" \
+			+ victim + "' AND FRIEND.user2 = '" + username + "' );";
+
+		//cout << "sql : " << sql << endl;
+		int rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else {
+			
+		}
+		
+	}
+
 	int isExisted(string username) {
-		bool check;
-		int  rc;
 		openDb();
 		string sql = "SELECT * FROM USER " \
 			"WHERE USER.ID = '" \
 			+ username + "';";
 
-		/* Execute SQL statement */
-		rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
-		if (rc != SQLITE_OK) {
-			fprintf(stderr, "SQL error: %s\n", zErrMsg);
-			sqlite3_free(zErrMsg);
-			check = false;
+		sqlite3_stmt *pSelectStatement = NULL;
+		int iResult = SQLITE_ERROR;
+		bool ret = false;
+
+		iResult = sqlite3_prepare_v2(db, sql.c_str(), -1, &pSelectStatement, 0);
+		if ((iResult == SQLITE_OK) && (pSelectStatement != NULL))
+		{
+			iResult = sqlite3_step(pSelectStatement);
+			//was found?
+			if (iResult == SQLITE_DONE) {
+				ret = true;
+				sqlite3_clear_bindings(pSelectStatement);
+				sqlite3_reset(pSelectStatement);
+			}
+			iResult = sqlite3_finalize(pSelectStatement);
 		}
-		else {
-			check = true;
-		}
-		sqlite3_close(db);
-		return check;
+		return ret;
 	}
 
-	
+
 
 	bool isBlocked(string sender, string receiver) {
 		openDb();
@@ -229,11 +358,42 @@ private:
 				ret =  false;
 			}
 		}
-		sqlite3_finalize(selectstmt);
+		sqlite3_close(db);
 		return ret;
 	}
 
-	
+	bool isFriend(string user1, string user2) {
+		openDb();
+		string sql = "SELECT * FROM FRIEND " \
+			"WHERE ( FRIEND.user1 = '" \
+			+ user1 + "' AND FRIEND.user2 = '" + user2 \
+			+ "' ) OR ( FRIEND.user1 = '" \
+			+ user2 + "' AND FRIEND.user2 = '" + user1 + "' );";
+
+		/* Execute SQL statement */
+		sqlite3_stmt *selectstmt = NULL;
+		int iResult = SQLITE_ERROR;
+		bool ret = true;
+		int result = sqlite3_prepare_v2(db, sql.c_str(), -1, &selectstmt, NULL);
+		if ((result == SQLITE_OK) && (selectstmt != NULL))
+		{
+			if (sqlite3_step(selectstmt) == SQLITE_ROW)
+			{
+				// record found
+			}
+			else
+			{
+				ret = false;
+			}
+		}
+		sqlite3_close(db);
+		return ret;
+	}
+
+	void clear_buffer() {
+		int ch;
+		while ((ch = getchar()) != '\n' && ch != EOF);
+	}
 
 };
 
